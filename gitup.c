e5eba291b5673b64f35ae3d150a1db0d1543d7fa
ondrejhonus <honusondrej@gmail.com>
2024-09-28 14:53:38 +0200

    does it work?

--- gitup.c
+++ gitup.c
@@ -2,16 +2,32 @@
 #include <string.h>
 #include <stdio.h>
 
-int main(void){
-    char usr_commit_msg[128] = "";
-    char commit_msg[156] = "";
-    system("git add .");
+#define MESSAGE_LENGHT 128
+#define COMMAND_SIZE 256
+
+int main(void) {
+    char usr_input_msg[MESSAGE_LENGHT] = "";
+    char commit_msg[COMMAND_SIZE] = "";
+    
+    // Prompt user for commit message
     printf("Type your commit message: ");
-    scanf("%s", usr_commit_msg);
+    fgets(usr_input_msg, MESSAGE_LENGHT, stdin);
+    
+    // Remove enter
+    if (MESSAGE_LENGHT > 0 && usr_input_msg[MESSAGE_LENGHT - 1] == '\n') {
+        usr_input_msg[MESSAGE_LENGHT - 1] = '\0';
+    }
+    
+    // Make commit command
     strcat(commit_msg, "git commit -m \"");
-    strcat(commit_msg, usr_commit_msg);
+    strcat(commit_msg, usr_input_msg);
     strcat(commit_msg, "\"");
-    system(commit_msg);
+    
+    //printf("%s\n", commit_msg);
+    
+    system("git add .");
+    system(commit_msg); // This executes "git commit -m "commit_msg""
     system("git push");
+
     return 0;
 }
