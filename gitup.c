a3dcc8fbf468e5a723135148dff273dccd22e447
ondrejhonus <honusondrej@gmail.com>
2024-09-29 09:08:25 +0200

    fix --set-upstream error

--- gitup.c
+++ gitup.c
@@ -1,5 +1,5 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define MESSAGE_LENGTH 128
 #define COMMAND_SIZE 256
@@ -7,18 +7,20 @@
 int main(void) {
     char usr_input_msg[MESSAGE_LENGTH] = "";
     char commit_msg[COMMAND_SIZE] = "";
-    
+
     // Check for an initialized git repo
     int is_git = system("git rev-parse --is-inside-work-tree > /dev/null 2>&1");
 
     if (is_git != 0) {
-        printf("Error: not a git repository.\nTry running \"git init\" to initialize it.\n");
+        printf(
+            "Error: not a git repository.\nTry running \"git init\" to "
+            "initialize it.\n");
         return 1;
     }
-    
+
     // Stage changes
     system("git add .");
-    
+
     // Check if there are any changes to commit
     int changes = system("git diff-index --quiet HEAD --");
     if (changes == 0) {
@@ -29,26 +31,29 @@ int main(void) {
     // Ask user for commit message
     printf("Type your commit message: ");
     fgets(usr_input_msg, MESSAGE_LENGTH, stdin);
-    
+
     // Make and run commit command
     snprintf(commit_msg, COMMAND_SIZE, "git commit -m \"%s\"", usr_input_msg);
-    system(commit_msg); // This executes "git commit -m "commit_msg""
-    
+    system(commit_msg);  // This executes "git commit -m "commit_msg""
+
     // Try executing git push
     int push_result = system("git push");
-    
+
     // If git push fails, then try to execute it with "--set-upstream"
     if (push_result != 0) {
         printf("Initial git push failed. Trying 'git push --set-upstream origin main'\n");
+        // Try pushing with --set-upstream
         push_result = system("git push --set-upstream origin main");
-    } else {
+        if (push_result != 0) {
+            printf("Couldn't push the commit to github. You're on your own now\n");
+            return 1;
+        } 
+        else {
+            printf("\nCommit pushed successfully! (using 'git push --set-upstream origin main')\n");
+        }
+    }
+    else {
         printf("\nCommit pushed successfully!\n");
     }
-    if (push_result != 0) {
-        printf("Couldn't push the commit to github. You're on your own now\n");
-    } else {
-        printf("\nCommit pushed successfully! (using 'git push --set-upstream origin main')\n");
-    }
-
     return 0;
 }
